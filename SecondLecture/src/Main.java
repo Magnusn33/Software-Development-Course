403cd4c8068197de6b5cc508593682076325607b
Magnus N. Soerensen <magnus@munchsorensen.dk>
2024-09-15 18:25:01 +0200

    15/09

--- SecondLecture/src/Main.java
+++ SecondLecture/src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Animal myre = new Ant();
+
+    System.out.println(myre.getLegs());
     }
-}   
+}
+
+abstract class Animal {
+    public static void main(String[] args) {
+    }
+    int getLegs() {
+        int legs = 0;
+
+        return legs;
+    }
+}
+
+class Human extends Animal {
+    @Override
+    int getLegs() {
+        int legs = 2;
+
+        return legs;
+    }
+}
+
+class Horse extends Animal {
+    @Override
+    int getLegs() {
+        int legs = 4;
+
+        return legs;
+    }
+}
+
+class Fish extends Animal {
+    @Override
+    int getLegs() {
+        int legs = 0;
+
+        return legs;
+    }
+}
+
+class Insect extends Animal {
+    @Override
+    int getLegs() {
+        int legs = 6;
+
+        return legs;
+    }
+}
+
+class Ant extends Insect {
+    @Override
+    int getLegs() {
+        return super.getLegs();
+    }
+}
