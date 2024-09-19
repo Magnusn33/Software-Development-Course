98ce80b968e7a27d57a1994dcb0ec3cb1728ecc9
Magnus N. Soerensen <magnus@munchsorensen.dk>
2024-09-19 13:41:26 +0200

    With a single unittest case

--- Lecture04/src/Main.java
+++ Lecture04/src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    static int add(int a, int b) {return a+b;}
 }
---
+++ Lecture04/src/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    void testAdd() {
+        assertEquals(4, Main.add(2,2));
+    }
+}
