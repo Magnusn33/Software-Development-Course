c5bf8425523fecaed937c59c35c71920f12316f4
Magnus N. Soerensen <magnus@munchsorensen.dk>
2024-09-24 09:33:30 +0200

    Add initial project structure with sample classes
    Created initial project with IntelliJ configuration files. Added basic `my_array` class with a method to check array equality and an empty `my_linkedList` class.

---
+++ Lection05/src/my_array.java
@@ -0,0 +1,17 @@
+public class my_array {
+    public static void main(String[] args) {
+        int[] aa = new int[1000];
+
+        System.out.println(allEq(aa));
+    }
+
+    static boolean allEq(int[] a){
+        if(a==null)return false;
+        for(int i=1;i<a.length;i++)if(a[i]!=a[i-1])return false;
+        return true;
+    }
+
+
+
+
+}
