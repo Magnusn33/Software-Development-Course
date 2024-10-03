ba485e055f270e912e829df1010416b31da54776
Magnus N. Soerensen <magnus@munchsorensen.dk>
2024-10-03 12:45:15 +0200

    Add ComboBox to JavaFX UI
    Included a ComboBox in the VBox layout alongside the existing button. This change improves interactivity by allowing users to select from predefined options. Additionally, adjusted the scene size to 500x500 for better layout control.

--- JavaFX_Template/src/main/java/com/example/javafx_template/HelloApplication.java
+++ JavaFX_Template/src/main/java/com/example/javafx_template/HelloApplication.java
@@ -1,21 +1,28 @@
 package com.example.javafx_template;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        Button button1=new Button("Click me!");
+        ComboBox<String> comboBox=new ComboBox<>();
+        comboBox.getItems().addAll("A","B","C");
+        comboBox.getItems().addAll("D");
+        VBox box=new VBox(button1,comboBox);
+        Scene scene = new Scene(box, 500, 500);
+        stage.setTitle("My JavaFX program");
         stage.setScene(scene);
         stage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
